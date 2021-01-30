73b4d766d14dfed86ab9fb4f63c2174848425680
Mengmei Tu <redmmeii@gmail.com>
2021-01-29 19:57:58 -0500

    deploy my react app to github pages

---
+++ src/components/TodoForm.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function TodoForm(props) {
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        inputRef.current.focus()
+    })
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 1000),
+            text: input
+        });
+
+        setInput('');
+    };
+
+    const handleChange = e => {
+        setInput(e.target.value);
+    }
+
+    return (
+        <form className="todo-form" onSubmit={handleSubmit}>
+            {props.edit ? (
+                <>
+                    <input
+                        type="text"
+                        placeholder="Update your item"
+                        value={input}
+                        name="text"
+                        className="todo-input edit"
+                        onChange={handleChange}
+                        ref={inputRef}
+                    />
+                    <button onClick={handleSubmit} className="todo-button edit">Update</button>
+                </>
+            ) : (
+                    <>
+                        <input
+                            type="text"
+                            placeholder="Add a todo"
+                            value={input}
+                            name="text"
+                            className="todo-input"
+                            onChange={handleChange}
+                            ref={inputRef}
+                        />
+                        <button onClick={handleSubmit} className="todo-button">Add todo</button>
+                    </>
+                )}
+        </form >
+    )
+}
+
+export default TodoForm;
